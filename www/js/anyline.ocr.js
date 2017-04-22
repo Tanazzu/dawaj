5fda4a2010a9fda90e8decd606dd3548d72b7724
Darek <tanazzu@gmial.com>
2017-04-22 17:08:11 +0200

    dawaj

---
+++ www/js/anyline.ocr.js
@@ -0,0 +1,109 @@
+/*
+ * Anyline Cordova Plugin
+ * anyline.ocr.js
+ *
+ * Copyright (c) 2016 9yards GmbH
+ */
+
+if (anyline === undefined) {
+    var anyline = {};
+}
+anyline.ocr = {
+    onResult: function(result) {
+        //this is called for every mrz scan result
+        //the result is a json-object containing all the scaned values and check-digits
+
+        console.log("Result: " + JSON.stringify(result));
+        var div = document.getElementById('results');
+
+        div.innerHTML = "<p>" + "<img src=\"" + result.imagePath + "\" width=\"100%\" height=\"auto\"/><br/>" +
+            "<b>Result: </b> " + result.text + "</p>" + div.innerHTML;
+
+        document.getElementById("details_scan_modes").removeAttribute("open");
+        document.getElementById("details_results").setAttribute("open", "");
+    },
+
+    onError: function(error) {
+        //called if an error occurred or the user canceled the scanning
+        if (error == "Canceled") {
+            //do stuff when user has canceled
+            // this can be used as an indicator that the user finished the scanning if canclelOnResult is false
+            console.log("AnylineOcr scanning canceled");
+            return;
+        }
+
+        alert(error);
+    },
+
+    licenseKey: "" +
+        "eyJzY29wZSI6WyJBTEwiXSwicGxhdGZvcm0iOlsiaU9TIiwiQW5kcm9pZCIsIldpbmRvd3" +
+        "MiXSwidmFsaWQiOiIyMDE3LTA1LTIyIiwibWFqb3JWZXJzaW9uIjoiMyIsImlzQ29tbWVy" +
+        "Y2lhbCI6ZmFsc2UsInRvbGVyYW5jZURheXMiOjYwLCJpb3NJZGVudGlmaWVyIjpbImFuZH" +
+        "JvaWQiXSwiYW5kcm9pZElkZW50aWZpZXIiOlsiYW5kcm9pZCJdLCJ3aW5kb3dzSWRlbnRp" +
+        "ZmllciI6WyJhbmRyb2lkIl19CnJGVjVPbGN3TmFoeTh4NnJtVVVSbDUxKzRiSENsRnpnS3" +
+        "FPcmVOa3NnVC84VmlzR29xa3czbi90NTFMdEVyam03ZDlpTDZTREdjQ3FPRVlEQ3RBeHov" +
+        "RnpsUkJUQXBHZEgzWHllTTRGaS9WajQ2S3lUQjR2eStkWGY5R2tRbmU3K3BYTWdvc0tYcG" +
+        "5IQXl5U0ZqMzRLY0FCaW9mbEFvUVVydEY0cWJUcDJLY1ErQ0JtYWhKTVJLd0tDYlcvbTNE" +
+        "NkFPL1BSY2wvTi96amFZdjFyeHNYMDJUdE9kcmNKSzRSdVhPOUw3R0Q0YmJRQ3ZWTGxWeW" +
+        "lzQzBYZ1hHdTNwaTI2eDlmaXh3b244YVVINTNoNEtVUWRHWDBKUmtWZVZTSWtmR1ZNejYv" +
+        "VGRIa1BtcTBodSs3aUt6ODliYlZVZWlqRFBJT0ljRkt3TE5QOVlMSk1VQ2FWUT09",
+
+    anylineVoucherCodesViewConfig: {
+        "captureResolution":"1080",
+        "cutout": {
+          "style": "rect",
+          "maxWidthPercent": "80%",
+          "maxHeightPercent": "80%",
+          "alignment": "center",
+          "width": 540,
+          "ratioFromSize": {
+            "width": 4,
+            "height": 1
+          },
+          "strokeWidth": 2,
+          "cornerRadius": 10,
+          "strokeColor": "FFFFFF",
+          "outerColor": "000000",
+          "outerAlpha": 0.3
+        },
+        "flash": {
+          "mode": "manual",
+          "alignment": "bottom_right"
+        },
+        "beepOnResult": true,
+        "vibrateOnResult": true,
+        "blinkAnimationOnResult": true,
+        "cancelOnResult": true,
+        "visualFeedback": {
+          "style": "contour_point",
+          "strokeColor": "0099FF",
+          "strokeWidth": 3
+        }
+      },
+
+    anylineVoucherCodesOcrConfig: {
+        "scanMode": "GRID",
+        "minCharHeight": 45,
+        "maxCharHeight": 85,
+        "traineddataFiles": ["assets/anyline_capitals.traineddata"],
+        "charWhitelist": "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        "validationRegex": "[A-Z0-9]{8}$",
+        "minConfidence": 85,
+        "removeSmallContours": true,
+        "charCountX": 8,
+        "charCountY": 1,
+        "charPaddingXFactor": 0.5,
+        "isBrightTextOnDark": true
+    },
+
+    scanAnylineVoucherCodes: function() {
+        // start the Anyline OCR scanning
+        // pass the success and error callbacks, as well as the license key and the config to the plugin
+        // see http://documentation.anyline.io/#anyline-config for config details
+        // and http://documentation.anyline.io/#anylineOcrModule for module details
+
+        cordova.exec(this.onResult, this.onError, "AnylineSDK", "ANYLINE_OCR", [this.licenseKey,
+            this.anylineVoucherCodesViewConfig, this.anylineVoucherCodesOcrConfig
+        ]);
+    }
+};
